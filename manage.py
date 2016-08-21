18908b5eb201ec60439a3ff534ddc5848c076f20
Brooks Kindle <brooks.kindle@wsu.edu>
2016-08-20 20:21:38 -0700

    Trying to debug TravisCI deploy script.

--- manage.py
+++ manage.py
@@ -13,8 +13,10 @@ manager = Manager(app)
 def freeze():
     """Freeze the website into a static version."""
     from flask_frozen import Freezer
+    print("freezing...", end="")
     freezer = Freezer(app)
     freezer.freeze()
+    print("done!")
 
 
 if __name__ == "__main__":
