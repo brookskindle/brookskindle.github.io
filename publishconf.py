9ea87ac9dae5c68bb9062c0cc41b77f23d2a8f2b
Brooks Kindle <brookskindle+git@gmail.com>
2018-08-25 10:15:33 -0700

    Remove atom feeds from site
    I don't use them

--- publishconf.py
+++ publishconf.py
@@ -11,9 +11,6 @@ from pelicanconf import *
 SITEURL = "https://brookskindle.github.io"
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = "feeds/all.atom.xml"
-CATEGORY_FEED_ATOM = "feeds/%s.atom.xml"
-
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
