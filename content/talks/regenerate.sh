1dd0a8a73f16b8b2eaac20fcb440e1d64a7f825f
Brooks Kindle <brookskindle+git@gmail.com>
2020-09-11 08:34:25 -0700

    Make regenerate.sh executable

--- content/talks/regenerate.sh
+++ content/talks/regenerate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
 # Useful with:
-#   echo cli-whirlwind-tour.md | entr bash regenerate.sh
+#   echo cli-whirlwind-tour.md | entr ./regenerate.sh
 
 cat remark-begin.html ./cli-whirlwind-tour.md ./remark-end.html > ./whirlwind.html
